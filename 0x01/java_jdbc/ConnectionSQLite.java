56759b822e2ab1105b20b8e25483f2191c111043
Suhailah de Moraes <68478385+SuhMoraes@users.noreply.github.com>
2022-03-24 11:34:12 -0300

    Update Class

--- 0x01/java_jdbc/ConnectionSQLite.java
+++ 0x01/java_jdbc/ConnectionSQLite.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class ConnectionSQLite {
+public class ConnectionSQLite {
+  public static void main(String[] args) {
+        initConnection();
+    }
+
+    public static void initConnection(){
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:sqlite_database_2022.db";
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
 }
